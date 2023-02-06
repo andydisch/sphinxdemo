b9539600e6b92cb2277be315ba783217c896c173
adisch <andy12@gmx.ch>
2023-02-06 11:43:17 +0100

    initial commit

--- sphinxdemo/customer.py
+++ sphinxdemo/customer.py
@@ -5,8 +5,8 @@ from enum import Enum
 
 
 class CustomerStatus(Enum):
-    """
-    Enumeration class to represent customer status.
+    """Enumeration class to represent customer status.
+
     """
 
     PREMIUM = "premium"
@@ -15,8 +15,8 @@ class CustomerStatus(Enum):
 
 @dataclass
 class BillItem:
-    """
-    Class to represent a bill item with price and quantity.
+    """Class to represent a bill item with price and quantity.
+
     """
 
     price: int
@@ -24,16 +24,16 @@ class BillItem:
 
     @property
     def total(self) -> int:
-        """
-        Returns the total amount for the bill item.
+        """Returns the total amount for the bill item.
+
         """
         return self.price * self.quantity
 
 
 @dataclass
 class Customer:
-    """
-    Class to represent a customer with name, bills and status.
+    """Class to represent a customer with name, bills and status.
+
     """
 
     name: str
@@ -42,13 +42,13 @@ class Customer:
 
     @property
     def open_bills(self) -> int:
-        """
-        Returns the total amount of all open bills.
+        """Returns the total amount of all open bills.
+
         """
         return sum(item.total for item in self.bills)
 
     def upgrade(self) -> None:
-        """
-        Upgrades customer status to premium.
+        """Upgrades customer status to premium.
+        
         """
         self.status = CustomerStatus.PREMIUM
